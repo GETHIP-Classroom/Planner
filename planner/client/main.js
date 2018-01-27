9ffe9faa0d1c8930e911735d07bd36025228bc9b
Owen G <ogong500@millardps.org>
2018-01-26 23:15:53 -0600

    basic dashboard

--- planner/client/main.js
+++ planner/client/main.js
@@ -1,22 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+$(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+  $(".side-nav .collapse").on("hide.bs.collapse", function() {                   
+      $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+  });
+  $('.side-nav .collapse').on("show.bs.collapse", function() {                        
+      $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");        
+  });
+})    
+  
